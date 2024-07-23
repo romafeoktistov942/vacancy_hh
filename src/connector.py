d963934dfe3703e280b5d34b455305485c4df0bf
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-07-23 21:36:19 +0300

    Implemented vacancy search feature with data retrieval, storage, and user interface integration

--- src/connector.py
+++ src/connector.py
@@ -1,6 +1,7 @@
 import json
 from abc import ABC, abstractmethod
 from src.vacancy import WorkVacancy
+import os
 
 
 class BaseConnector(ABC):
@@ -21,38 +22,72 @@ class BaseConnector(ABC):
     def delete_vacancy(self) -> None:
         pass
 
+    @staticmethod
+    def filter_vacancy(
+        dict_vacancy: dict, key_name: str, value_name: int | str
+    ):
+        pass
+
 
 class Connector(BaseConnector):
 
-    def __init__(self) -> None:
+    def __init__(self, name_file: str) -> None:
         self._vacancy_list = []
         self._finish_list = []
+        self.name_file = name_file
+
+    @staticmethod
+    def filter_vacancy(vacancy_dict: dict, key: str, value: int | str) -> bool:
+        return vacancy_dict[key] == value
 
     def create_vacancy_list(self) -> list:
         """
         Метод класса для формирования списка вакансий по новому
+        :param name_file: название файла, в котором хранится спаршенная информация с hh.ru
         :return: список вакансий после обработки классом WorkVacancy
         """
 
-        with open("./vacancy_hh/data/vacancies.json", "r", encoding="utf-8") as file:
+        with open(self.name_file, "r", encoding="utf-8") as file:
             read_vacancy_file = json.load(file)
             for item in read_vacancy_file:
                 if item["salary"] is None or item["area"] is None:
                     continue
                 else:
-                    self._vacancy_list.append(WorkVacancy(item["name"], item["alternate_url"], item["area"]["name"],
-                                                          item["salary"]["from"], item["salary"]["to"],
-                                                          item["salary"]["currency"], item["snippet"]["requirement"]))
+                    self._vacancy_list.append(
+                        WorkVacancy(
+                            item["name"],
+                            item["alternate_url"],
+                            item["area"]["name"],
+                            item["salary"]["from"],
+                            item["salary"]["to"],
+                            item["salary"]["currency"],
+                            item["snippet"]["requirement"],
+                        )
+                    )
         return self._vacancy_list
 
     def add_vacancy(self) -> None:
         vacancy_list = self.create_vacancy_list()
         new_vac = []
-        with open("./vacancy_hh/data/vacancies_to_work.json", "w", encoding="utf-8") as file:
+
+        with open(self.name_file, "w", encoding="utf-8") as file:
             for f in vacancy_list:
-                new_vac.append({"name": f.name_vacancy, "url": f.url_vacancy, "area": f.city,
-                                "salary_from": f.salary_from, "salary_to": f.salary_to,
-                                "currency": f.salary_currency, "snippet": f.snippet_requirement})
+                salary_from = f.salary_from
+                salary_to = f.salary_to
+                currency = f.salary_currency
+
+                new_vac.append(
+                    {
+                        "name": f.name_vacancy,
+                        "url": f.url_vacancy,
+                        "area": f.city,
+                        "salary_from": salary_from,
+                        "salary_to": salary_to,
+                        "currency": currency,
+                        "snippet": f.snippet_requirement,
+                    }
+                )
+
             return json.dump(new_vac, file, indent=4)
 
     def get_info(self, key_name: str, value_name: str | int) -> list:
@@ -64,24 +99,18 @@ class Connector(BaseConnector):
                            получаемый от пользователя. Для осуществления подбора вакансий
         :return: список отфильтрованных вакансий в соответствии с заданным условием
         """
-        with open("./vacancy_hh/data/vacancies_to_work.json", "r", encoding="utf-8") as file:
+        with open(self.name_file, "r", encoding="utf-8") as file:
             top_list = json.load(file)
+            for item in top_list:
+                if self.filter_vacancy(item, key_name, value_name) is True:
+                    self._finish_list.append(item)
+                else:
+                    continue
+            return self._finish_list
 
-        top_ = sorted(top_list, key=lambda x: x[key_name], reverse=True)
-
-        for item in top_:
-            if value_name == item[key_name]:
-                self._finish_list.append(item)
-
-        return self._finish_list
+    def __str__(self, *args):
+        return f"{self.get_info(*args)}"
 
     def delete_vacancy(self) -> None:
-        """
-        Метод удаляющий содержимое файлов: базового, с вакансиями полученными с hh.ru;
-        с вакансиями для сортировки.
-        """
-        with open("./vacancy_hh/data/vacancies_to_work.json", "w", encoding="utf-8") as _:
-            pass
-
-        with open("./vacancy_hh/data/vacancies.json", "w", encoding="utf-8") as _:
-            pass
+        """Метод удаляющий файл *.json содержащий вакансии сформированный в соответсвии с заданной структурой"""
+        os.remove(self.name_file)
