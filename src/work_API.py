947d9319f9c8c8d60d6fe7fa0908ed5be4845a04
Roman Feoktistov <romafeoktistov942@gmail.com>
2024-08-19 20:58:56 +0300

    update work api

--- src/work_API.py
+++ src/work_API.py
@@ -1,51 +1,101 @@
-import json
 import requests
+import json
 from abc import ABC, abstractmethod
 
 
-class BaseHH(ABC):
-    """Класс-конструктор для работы с API HH"""
+class BaseParser(ABC):
 
     @abstractmethod
-    def __init__(self):
+    def load_file(self):
+        pass
+
+    @abstractmethod
+    def save_file(self):
         pass
 
 
-class Parser(BaseHH):
+class ParserEmployer(BaseParser):
+    """Класс получающий информацию о работодателе по запросу пользователя."""
 
-    def __init__(self, file_worker) -> None:
-        self.file_worker = file_worker
+    employer = []
 
-    def save_in_file(self, vacancies) -> None:
-        """
-        Метод класса Parser(BaseHH) записывающий в файл *.json всю информацию по вакансиям
-        :param vacancies: список вакансий полученных с сайта hh.ru по запросу
-        """
-        with open(self.file_worker, "w", encoding="utf-8") as file:
-            json.dump(vacancies, file, indent=4)
-
-
-class HH(Parser):
-    """Класс для работы с API HeadHunter"""
-
-    def __init__(self, file_worker) -> None:
-        self.url = "https://api.hh.ru/vacancies"
+    def __init__(
+        self, name_file_company: str, name_company: str, max_page: int = 20
+    ) -> None:
+        self.name_file_company = name_file_company
+        self.name_company = name_company
+        self.max_page = max_page
         self.headers = {"User-Agent": "HH-User-Agent"}
         self.params = {"text": "", "page": 0, "per_page": 100}
-        self.vacancies = []
-        super().__init__(file_worker)
+        self.url_company = f"https://api.hh.ru/employers"
 
-    def load_vacancies(self, keyword) -> None:
-        """
-        Метод класса HH(Parser), который запрашивает информацию по вакаснсиям на сайте hh.ru
-        :param keyword: наименовании запрашиваемой вакансии
-        """
-        self.params["text"] = keyword
-        while self.params.get("page") != 20:
-            response = requests.get(
-                self.url, headers=self.headers, params=self.params, timeout=10
+    def load_file(self):
+        """Метод получающий информацию по работодетелю, у которого есть активные вакансии."""
+        self.params["text"] = self.name_company
+
+        for param in range(0, self.max_page):
+            self.params["page"] = param
+            responce = requests.get(
+                self.url_company,
+                headers=self.headers,
+                params=self.params,
+                timeout=10,
             )
-            vacancies = response.json()["items"]
-            self.vacancies.extend(vacancies)
+            company = responce.json()["items"]
+            responce.raise_for_status()
+
+            for item in company:
+                if item["open_vacancies"] > 0:
+                    ParserEmployer.employer.append(item)
+                else:
+                    continue
+
+    def save_file(self) -> None:
+        """Метод сохраняющий информацию по работодателяю в файл json."""
+        with open(self.name_file_company, "w", encoding="utf-8") as file:
+            json.dump(ParserEmployer.employer, file, indent=4)
+
+
+class ParserEmployerVacancy(BaseParser):
+    """Класс получающий информацию о вакансиях работодателя"""
+
+    vacancies_employer = []
+
+    def __init__(
+        self, name_file_vacancy: str, id_employer: int, max_page: int = 20
+    ) -> None:
+
+        self.name_file_vac = name_file_vacancy
+        self.id_empl = id_employer
+        self.max_page = max_page
+        self.headers = {"User-Agent": "HH-User-Agent"}
+        self.params = {"page": 0, "per_page": 100}
+        self.url_vac_emp = (
+            f"https://api.hh.ru/vacancies?employer_id={self.id_empl}"
+        )
+
+    def load_file(self) -> None:
+        """Метод получающий информацию о вакансиях работодетеля"""
+        while self.params.get("page") != 20:
+
+            responce = requests.get(
+                self.url_vac_emp,
+                headers=self.headers,
+                params=self.params,
+                timeout=10,
+            )
+
+            if (
+                not responce.json().get("items")
+                or responce.json().get("items") is []
+            ):
+                break
+            else:
+                vacancy = responce.json()["items"]
+                ParserEmployerVacancy.vacancies_employer.extend(vacancy)
             self.params["page"] += 1
-        super().save_in_file(self.vacancies)
+
+    def save_file(self):
+        """Метод сохраняющий информацию о вакансиях работодателя в файл json."""
+        with open(self.name_file_vac, "w", encoding="utf-8") as file:
+            json.dump(self.vacancies_employer, file, indent=4)
